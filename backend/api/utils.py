90988abd69fb33880ea94b9970c52a5ada9a2dba
Dmitry Semenov <kamstrim@mail.ru>
2023-08-09 00:58:33 +0300

    isort fix v3

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,7 +1,6 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
-
-from django.shortcuts import get_object_or_404
 
 from .models import Ingredient, RecipeIngredient
 
